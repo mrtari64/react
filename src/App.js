3af2354c5be2e4c072fa76ecd74a5606ecb4f84d
tari <sds@dfd.com>
2022-09-12 10:04:06 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import CardList  from './components/card-list/card-list.component';
+import  SearchBox  from "./components/search-box/search-box.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchItem:'',
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/USERS")
+      .then((responce) => responce.json())
+      .then((users) =>
+        this.setState(() => {
+          
+          return { monsters: users };
+        })
+      );
+  }
+onSearchChange=
+  (event) => {
+    this.setState(() => {
+      return { searchItem: event.target.value }; //{searchItem:e.target.value}
+    });
+  }
+
+
+  render() {
+    const {onSearchChange}=this;
+     const result=this.findMonster()
+    
+
+    return (
+      <div className="App">
+        {/* {result.map((fmonster) => {
+          console.log(fmonster.name);
+          return <h1 key={fmonster.id}>{fmonster.name}</h1>;
+        })} */}
+        <h1 className="app-title">Monster Rolex</h1>
+        <SearchBox
+          className='search-box'
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+        />
+        <CardList monsters={result} />
+      </div>
+    );
+  }
+
+  findMonster() {
+    const {monsters,searchItem}=this.state;
+   // console.log("6666"+searchItem.toString());
+    const filtredMonster = monsters.filter((monster) =>
+      monster.name
+        .toLowerCase()
+        .includes(searchItem.toString().toLowerCase())
+    );
+    //console.log("wwww"+ filtredMonster[0].name);
+return filtredMonster;
+  }
 }
 
 export default App;
